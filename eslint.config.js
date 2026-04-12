7efd2ddaaf6c034b5387c56e65a264dbca22b2fb
paul schuhmacher <contact@pschuhmacher.com>
2026-04-12 17:04:41 +0200

    reset eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,47 @@
 const js = require("@eslint/js");
 const globals = require("globals");
+const sonarjs = require("eslint-plugin-sonarjs");
 
 module.exports = [
+  // base ESLint recommended rules
   js.configs.recommended,
 
   {
     files: ["**/*.js"],
 
     languageOptions: {
+      ecmaVersion: "latest",
       sourceType: "commonjs",
       globals: {
-        ...globals.node,
-        ...globals.jest
+        ...globals.node
       }
     },
 
+    plugins: {
+      sonarjs
+    },
+
     rules: {
+      /*
+       * === CORE SAFETY ===
+       */
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-undef": "error",
       "no-console": "warn",
-      "no-unused-vars": "error",
-      "eqeqeq": "error"
+      "eqeqeq": "error",
+      "curly": "error",
+
+      /*
+       * === NODE FRIENDLY ===
+       */
+      "no-process-exit": "off",
+
+      /*
+       * === SONARJS (quality / smells) ===
+       */
+      "sonarjs/cognitive-complexity": ["warn", 5],
+      "sonarjs/no-identical-functions": "error",
+      "sonarjs/no-redundant-boolean": "error"
     }
   }
 ];
